c2c65f2ee6d38ac25c9555f932686ca7ef571e1b
Jtandoh01 <tandoh24700@gmail.com>
2023-07-23 07:49:25 +0000

    project setup and structure

---
+++ HomeActivity.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const HomeActivity = () => {
+  return (
+    <SafeAreaView>
+        <Text>Home</Text>
+    </SafeAreaView>
+  )
+};
+
+export default HomeActivity;
---
+++ RestaurantActivtiy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RestaurantActivtiy = () => {
+  return (
+    <Text>Restaurant</Text>
+  )
+}
+
+export default RestaurantActivtiy
