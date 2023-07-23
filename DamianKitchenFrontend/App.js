09246773f0c46aeaf634795afcbec923dbddf57d
Jtandoh01 <tandoh24700@gmail.com>
2023-07-23 05:33:55 +0000

    specified paths to image resources

--- DamianKitchenFrontend/App.js
+++ DamianKitchenFrontend/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import {  Text, View } from 'react-native';
+import Navigation from './navigation';
 
 export default function App() {
   return (
-    <View >
-      <Text className="text-blue-500">Hello Wolrd!</Text>
-      <StatusBar  />
-    </View>
+    <Navigation/>
   );
 }
 
